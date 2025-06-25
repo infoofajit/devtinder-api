5dec4e64bbceac6a61fa4f743e724e9cf32a646a
infoofajit <infoofajit@gmail.com>
2025-06-25 11:13:35 +0530

    Explore GET/POST/DELETE route

--- src/app.js
+++ src/app.js
@@ -2,16 +2,23 @@ const express = require('express');
 
 const app = express();
 
+// This will only handle get calls to /user
+app.get('/user', (req, res) => {
+  res.send({ firstname: 'Ajit', lastname: 'Singh' })
+})
+
+app.post('/user', (req, res) => {
+  // Saving data to DB
+  res.send("Data saved successfully to the DB!")
+})
+
+app.delete('/user', (req, res) => {
+  res.send("Deleted successfully!")
+})
+
+// This route will match all HTTP methods API calls to /test
 app.use('/test', (req, res) => {
-  res.send('Hey from test route!')
-})
-
-app.use('/secret', (req, res) => {
-  res.send('Hey! There is no secret')
-})
-
-app.use('/', (req, res) => {
-  res.send('Hey from Dashboard!')
+  res.send('Test hello from the server!')
 })
 
 app.listen(8000, () => {
